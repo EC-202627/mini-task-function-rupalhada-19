1a4aefbac2314bb35360800d6eac71f0c60f1c0e
rupalhada-19 <rupal.hada@avantika.edu.in>
2026-03-28 16:53:41 +0000

    completed mini task function

--- Problem_04.py
+++ Problem_04.py
@@ -1,15 +1,14 @@
-def calculate_fine(book_title ,days_overdue, daily_rate=10.0, max_fine=150.0):
+def calculate_fine(book_title, days_overdue, daily_rate=5.0, max_fine=150.0):
     fine = days_overdue * daily_rate
-    if fine >max_fine:
+    if fine > max_fine:
         fine = max_fine
-    return fine
-
+    return fine 
 book_title = input()
 days_overdue = int(input())
-daily_rate= float(input())
+daily_rate = float(input())
+fine = calculate_fine(book_title, days_overdue, daily_rate)
 
-fine = calculate_fine(book_title,days_overdue,daily_rate)
-print("Book:",book_title)
-print("Days overdue:",days_overdue)
-print("fine: Rs.",float(fine))
+print("Book:", book_title)
+print("Days overdue:", days_overdue)
+print("Fine: Rs.", float(fine))
 
